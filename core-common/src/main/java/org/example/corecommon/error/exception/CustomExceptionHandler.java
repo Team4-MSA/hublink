9b92e2bd3ccc316903b035b0e75287289295b59b
kimyounguk1 <kyw020108@gmail.com>
2026-05-18 16:20:59 +0900

    feat: 공통 응답 예외 구현

---
+++ core-common/src/main/java/org/example/corecommon/error/exception/CustomExceptionHandler.java
@@ -0,0 +1,41 @@
+package org.example.corecommon.error.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.example.corecommon.error.ErrorResponse;
+import org.example.corecommon.response.GlobalResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<GlobalResponse> handleCustomException(CustomException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse errorResponse = ErrorResponse.of(errorCode.name(), e.getMessage());
+        final GlobalResponse response = GlobalResponse.failure(errorCode.getStatus().value(), errorCode.name() ,errorResponse);
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GlobalResponse> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorResponse errorResponse = ErrorResponse.of("VALIDATION_ERROR", errors.toString());
+        GlobalResponse response = GlobalResponse.failure(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(), errorResponse);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+
+}
--- user-service/src/main/java/com/msa/user_service/refresh/RefreshController.java
+++ user-service/src/main/java/com/msa/user_service/refresh/RefreshController.java
@@ -1,10 +1,14 @@
 package com.msa.user_service.refresh;
 
+import com.msa.user_service.dto.ExamDto;
+import org.example.corecommon.error.exception.CustomException;
+import org.example.corecommon.error.exception.ErrorCode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RefreshScope
 @RestController
@@ -17,5 +21,20 @@ public class RefreshController {
     @GetMapping("/msg")
     public String msg() {
         return message;
+    }
+
+    @GetMapping("/test")
+    public Map<String, String> test() {
+        return Map.of("msg", message);
+    }
+
+    @GetMapping("/errorTest")
+    public void errorTest() {
+        throw new CustomException(ErrorCode.TEST_ERROR_CODE);
+    }
+
+    @PostMapping("/validTest")
+    public void validTest (@RequestBody @Validated ExamDto examDto){
+
     }
 }

09439cfa0369553eba26513b3d2dace3c5268a25
jun-geun-choi <jklucky73@gmail.com>
2026-05-22 13:54:01 +0900

    260522-13:53-mod-Product 일부 수정

--- product-service/src/main/java/com/msa/product_service/client/UserClient.java
+++ product-service/src/main/java/com/msa/product_service/client/UserClient.java
@@ -1,17 +1,20 @@
 package com.msa.product_service.client;
 
 import com.msa.core_common.response.GlobalResponse;
+import java.util.Map;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @FeignClient(name = "user-service")
 public interface UserClient {
     // 주소는 추후에 추가할 예정
     // 허브 담당자 본인 확인 여부
-    @GetMapping("~~")
-    GlobalResponse<Boolean> IsHubManager(UUID userId, UUID hubId);
+    @GetMapping("/internal/users/{userId}/hub/{hubId}/verify")
+    GlobalResponse<Map<String,Boolean>> IsHubManager(@PathVariable("userId")UUID userId, @PathVariable("hubId")UUID hubId);
     // 업체 담당자 본인 확인 여부
-    @GetMapping("~~")
-    GlobalResponse<Boolean> IsCompanyManager(UUID userId, UUID companyId);
+    @GetMapping("/internal/users/{userId}/company/{companyId}/verify")
+    GlobalResponse<Map<String,Boolean>> IsCompanyManager(@PathVariable("userId")UUID userId, @PathVariable("companyId")UUID companyId);
 }

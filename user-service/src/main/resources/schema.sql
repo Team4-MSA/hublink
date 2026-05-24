2ea50276d609053eb6c51b15f2e12ed690b8510b
LeeSG-0114 <leesin0114@naver.com>
2026-05-24 16:18:58 +0900

    refactor: 멱등성 처리

--- user-service/src/main/resources/schema.sql
+++ user-service/src/main/resources/schema.sql
@@ -1,5 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS user_service;
 
--- 허브별 배송 순번 유니크 제약조건 (동시 INSERT 시 중복 방지)
-ALTER TABLE user_service.p_delivery_managers
-    ADD CONSTRAINT uk_hub_delivery_sequence UNIQUE (hub_id, delivery_sequence);
+-- 허브별 배송 순번 유니크 제약조건
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint WHERE conname = 'uk_hub_delivery_sequence'
+    ) THEN
+        ALTER TABLE user_service.p_delivery_managers
+            ADD CONSTRAINT uk_hub_delivery_sequence UNIQUE (hub_id, delivery_sequence);
+    END IF;
+END $$;

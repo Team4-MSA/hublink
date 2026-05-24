f8ce792a95cb4855767b78cb22e7ebc6b14e9df3
LeeSG-0114 <leesin0114@naver.com>
2026-05-24 16:07:18 +0900

    refactor: sql에서 유니크 제약 설정

--- user-service/src/main/resources/schema.sql
+++ user-service/src/main/resources/schema.sql
@@ -1,1 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS user_service;
+
+-- 허브별 배송 순번 유니크 제약조건 (동시 INSERT 시 중복 방지)
+ALTER TABLE user_service.p_delivery_managers
+    ADD CONSTRAINT uk_hub_delivery_sequence UNIQUE (hub_id, delivery_sequence);

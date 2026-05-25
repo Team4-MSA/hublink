088ee821959ce15ce5860245928da806626458a6
tlsdud135 <kytbible@gmail.com>
2026-05-26 05:13:28 +0900

    feat: 내부 통신 api 구현, 서킷브레이커 적용, zipkin, swagger설정 추가

--- company-service/src/main/java/com/msa/company_service/CompanyServiceApplication.java
+++ company-service/src/main/java/com/msa/company_service/CompanyServiceApplication.java
@@ -2,15 +2,16 @@ package com.msa.company_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.msa"})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CompanyServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompanyServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyServiceApplication.class, args);
+    }
 
 }
---
+++ company-service/src/main/java/com/msa/company_service/client/HubClient.java
@@ -0,0 +1,19 @@
+package com.msa.company_service.client;
+
+import com.msa.company_service.dto.CoordinateDto;
+import com.msa.core_common.response.GlobalResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
+
+@FeignClient(name = "hub-service", fallbackFactory = HubClientFallbackFactory.class)
+public interface HubClient {
+    @GetMapping("/internal/hubs/{hubId}/exists")
+    GlobalResponse<Boolean> getHubExist(@PathVariable UUID hubId);
+
+    @GetMapping("/internal/hubs/coordinates")
+    GlobalResponse<CoordinateDto> getCoordinates(@RequestParam("address") String address);
+}

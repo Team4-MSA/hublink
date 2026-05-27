da25f51209a4b6a753cd6eac9693ca888ea4ca60
hyeeuncho <heeuncho@naver.com>
2026-05-27 13:26:20 +0900

    refactor: 수정사항 반영

--- order-service/src/main/java/com/msa/order_service/config/OpenApiConfig.java
+++ order-service/src/main/java/com/msa/order_service/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,9 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+    @Value("${swagger.gateway-url}")
+    private String gatewayUrl;
+
     @Bean
     public OpenAPI openAPI() {
         String securitySchemeName = "BearerAuth";
@@ -23,7 +27,7 @@ public class OpenApiConfig {
                         .version("v1"))
                 .servers(List.of(
                         new Server()
-                                .url("http://localhost:19091")
+                                .url(gatewayUrl)
                                 .description("API Gateway")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))

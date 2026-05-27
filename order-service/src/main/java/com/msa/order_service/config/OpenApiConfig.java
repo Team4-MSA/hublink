c325b1cb99a2ab7db8c47b96b56dd4d4c830a0cb
hyeeuncho <heeuncho@naver.com>
2026-05-27 13:09:15 +0900

    refactor: swagger에 토큰 추가

--- order-service/src/main/java/com/msa/order_service/config/OpenApiConfig.java
+++ order-service/src/main/java/com/msa/order_service/config/OpenApiConfig.java
@@ -1,6 +1,10 @@
 package com.msa.order_service.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +15,26 @@ import java.util.List;
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "BearerAuth";
         return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .description("Order 서비스 API 문서")
+                        .version("v1"))
                 .servers(List.of(
                         new Server()
                                 .url("http://localhost:19091")
                                 .description("API Gateway")
-                ));
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
